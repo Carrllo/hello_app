2866c5334166e229a8e2214aee77218890d92b72
Mrarcin22x <marcin22x@gmail.com>
2019-01-08 23:35:02 +0100

    Zapetlenie programu, przyjmowanie polecen tekstowych

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
 
 int main(int argc , char** argv)
 {
-	printf("Skromne \"hello!\"!");
+	std::cout << "Prosty kalkulator - 2019 :)\n"
+			  << "===========================\n\n";
+
+	std::string cmd_str;
+	while (true)
+	{
+		std::cout << "Podaj polecenie (help - pomoc, exit - wyjscie): ";
+		std::cin >> cmd_str;
+
+		// EXIT
+		if (cmd_str == "exit" || cmd_str == "0")
+		{
+			std::cout << "Koniec pracy!\n";
+			break;
+		}
+	}
+	
 	return 0;
 }
