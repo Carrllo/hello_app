7563f58b4ba2089b4bab6d5b2448576424be8385
mrxxamo <mrx4mo@gmail.com>
2019-01-09 23:28:47 +0100

    czystosc kodu+polskie znaki

--- dodawanie.cpp
+++ dodawanie.cpp
@@ -1,18 +1,23 @@
-#include <conio.h>
+#include <iostream>
 
 using namespace std;
 
 int dodawanie()
 {
 
+	setlocale(LC_ALL, ");
+
 	float a,b,c;
+
 	cout <<"Liczenie sumy dwóch liczb" << endl;
+
 	cout << "Podaj liczbę A: ";
 		cin >> a;
+
 	cout << endl << "Podaj liczbę B: ";
 		cin >> b;
 
 	c = a+b;
-	cout << endl <<  "Suma tych Liczb wynosi: " << c; 
+	cout << endl <<  "Suma A(" << a << ") i B(" << b << ") wynosi: " << c; 
 
 }
