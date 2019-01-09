1bcd0e581e5df65c0dc19e8c49018c853641dec5
batoff <wojciech.wawron@gmail.com>
2019-01-09 19:57:57 +0100

    Dodałem namespace std żeby mniej pisać i usunąłem niepotrzebne std

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main(int argc , char** argv)
 {
-	std::cout << "Prosty kalkulator - 2019 :)\n"
+	cout << "Prosty kalkulator - 2019 :)\n"
 			  << "===========================\n\n";
 
-	std::string cmd_str;
+	string cmd_str;
 	while (true)
 	{
-		std::cout << "Podaj polecenie (help - pomoc, exit - wyjscie): ";
-		std::cin >> cmd_str;
+		cout << "Podaj polecenie (help - pomoc, exit - wyjscie): ";
+		cin >> cmd_str;
 
 		// EXIT
 		if (cmd_str == "exit" || cmd_str == "0")
 		{
-			std::cout << "Koniec pracy!\n";
+			cout << "Koniec pracy!\n";
 			break;
 		}
 	}
