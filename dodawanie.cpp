c1a974b1fd137aa62f2bd7deb2e83379bda04672
mrxxamo <mrx4mo@gmail.com>
2019-01-09 23:21:09 +0100

    dodanie nagłówka

--- dodawanie.cpp
+++ dodawanie.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int dodawanie()
 {
 
-int a;
-int b;
-int c;
+	int a;
+	int b;
+	int c;
+	cout <<"Liczenie sumy dwóch liczb" << endl;
+	cout << "Podaj liczbę A: ";
+		cin >> a;
+	cout << endl << "Podaj liczbę B: ";
+		cin >> b;
 
-cout << "Podaj liczbę A: ";
-cin >> a;
-cout <<"Podaj liczbę B: ";
-cin >> b;
-
-c = a+b;
-cout << "suma wynosi :" << c; 
+	c = a+b;
+	cout << endl <<  "Sumy tych Liczb wynosi: " << c; 
 
 }
